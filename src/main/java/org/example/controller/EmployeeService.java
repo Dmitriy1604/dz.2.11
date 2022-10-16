45bec2212853f990dde021588caf9beb4e150bd1
Dmitriy1604 <Dimon312-91@mail.ru>
2022-10-16 14:46:42 +0300

    выполнил

---
+++ src/main/java/org/example/controller/EmployeeService.java
@@ -0,0 +1,45 @@
+package org.example.controller;
+
+import java.util.List;
+
+public class EmployeeService {
+    public org.example.service.Employee addEmployee(String firstName , String lastName , int salary , int department) {
+
+
+        return null;
+    }
+
+    public org.example.service.Employee removeEmployee(String firstName , String lastName) {
+
+        return null;
+    }
+
+    public org.example.service.Employee findEmployee(String firstName , String lastName) {
+        return null;
+    }
+
+    public org.example.service.Employee getHighestPaidEmployee(int departmentId) {
+        return null;
+    }
+
+    public org.example.service.Employee getLowestPaidEmployee(int departmentId) {
+        return null;
+    }
+
+    public List<org.example.service.Employee> printEmployeesForDepartment(int departmentId) {
+        return null;
+    }
+
+    public List<org.example.service.Employee> printEmployeesByDepartments() {
+        return null;
+    }
+
+    public List<org.example.service.Employee> printEmployees() {
+        return null;
+    }
+
+    public List<org.example.service.Employee> fillEmployeesList() {
+        final List<Employee> o = null;
+        return null;
+    }
+}
